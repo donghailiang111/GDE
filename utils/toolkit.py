4a5717b49c9e59a15d43251e8bd0cba8be1ba7ad
董海亮 <80665370+donghailiang111@users.noreply.github.com>
2026-05-07 20:12:06 +0800

    Update toolkit.py

--- utils/toolkit.py
+++ utils/toolkit.py
@@ -12,18 +12,7 @@ def mean_average_precision(query_code,
                            device,
                            topk=None,
                            ):
-    """
-    Calculate mean average precision(map).
-    Args:
-        query_code (torch.Tensor): Query data hash code.
-        database_code (torch.Tensor): Database data hash code.
-        query_labels (torch.Tensor): Query data targets, one-hot
-        database_labels (torch.Tensor): Database data targets, one-host
-        device (torch.device): Using CPU or GPU.
-        topk (int): Calculate top k data map.
-    Returns:
-        meanAP (float): Mean Average Precision.
-    """
+
     if topk == None:
         topk = database_code.shape[0]
 
@@ -31,26 +20,19 @@ def mean_average_precision(query_code,
     mean_AP = 0.0
 
     for i in range(num_query):
-        # Retrieve images from database
+       
         retrieval = (query_labels[i, :] @ database_labels.t() > 0).float()
 
-        # Calculate hamming distance
         hamming_dist = 0.5 * (database_code.shape[1] - query_code[i, :] @ database_code.t())
 
-        # Arrange position according to hamming distance
         retrieval = retrieval[torch.argsort(hamming_dist)][:topk]
 
-        # Retrieval count
         retrieval_cnt = retrieval.sum().int().item()
 
-        # Can not retrieve images
         if retrieval_cnt == 0:
             continue
-
-        # Generate score for every position
         score = torch.linspace(1, retrieval_cnt, retrieval_cnt).to(device)
 
-        # Acquire index
         index = (torch.nonzero(retrieval == 1, as_tuple=False).squeeze() + 1.0).float()
 
         mean_AP += (score / index).mean()
@@ -83,7 +65,7 @@ def makedirs(path):
 
 
 def split_images_labels(imgs):
-    # split trainset.imgs in ImageFolder
+    
     images = []
     labels = []
     for item in imgs:
